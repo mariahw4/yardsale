bcccdeb00d8b0c79b194759158db2edf556a2608
Mariah Wear <wearmariahk@gmail.com>
2023-03-23 20:23:34 -0600

    updating routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Blog, User, Comment } = require('../models');
+const { Listing , User } = require('../models');
 const withAuth = require('../utils/auth');
 
+
+
+
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.loggedIn) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
+  
