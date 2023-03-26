9547aae17ec396439e4c1245f47f3a1f77379acb
rhubarb414 <ctophr.dubois@gmail.com>
2023-03-26 10:10:00 -0700

    Adds placeholder route for / in homeRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const listingRoutes = require("./listingRoutes");
 
 router.use("/users", userRoutes);
 router.use("/listings", listingRoutes);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,21 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Listing , User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Listing, User } = require("../models");
+const withAuth = require("../utils/auth");
 
+// Placeholder route for homepage
+router.get("/", (req, res) => {
+  res.json("Home route");
+});
 
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.loggedIn) {
+    res.redirect("/profile");
+    return;
+  }
 
+  res.render("login");
+});
 
-
-router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.loggedIn) {
-      res.redirect('/profile');
-      return;
-    }
-  
-    res.render('login');
-  });
-  
-  module.exports = router;
-  
+module.exports = router;
