d6622ede0cf9f71513a6f22d118234b93d2ab2c2
rhubarb414 <ctophr.dubois@gmail.com>
2023-03-26 10:04:24 -0700

    Adds /listings route to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,7 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
 
-
-router.use('/users', userRoutes);
-
+router.use("/users", userRoutes);
+router.use("/listings", listingRoutes);
 
 module.exports = router;
