bf0206ab8727410b6d5f9697e6ee0210754c3cc0
Mariah Wear <wearmariahk@gmail.com>
2023-03-27 22:38:34 -0600

    updating routes for login and logout

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -60,12 +60,12 @@ router.post('/', async (req, res) => {
 // login route
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne({ where: { name: req.body.name } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+        .json({ message: 'Incorrect name or password, please try again' });
       return;
     }
 
@@ -74,13 +74,14 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+        .json({ message: 'Incorrect name or password, please try again' });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.username = userData.username;
+      req.session.name = userData.name;
+      req.session.email = userData.email;
       req.session.loggedIn = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
