443b0100f11ca676e1384698594f7a4427708864
Jsrra <josephserrato10@gmail.com>
2023-03-27 19:58:56 -0700

    adds working home route and seed render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,18 +4,44 @@ const { Listing, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 // Placeholder route for homepage
-router.get("/", (req, res) => {
-  res.json("Home route");
-});
+// router.get("/", (req, res) => {
+//   res.json("Home route");
+// });
 
-router.get("/login", (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.loggedIn) {
-    res.redirect("/profile");
-    return;
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const listingData = await Listing.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const listings = listingData.map((listing) => listing.get({ plain: true }));
+    console.log("Listings:", listings);
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      listings,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.render("login");
 });
+
+// router.get("/login", (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.loggedIn) {
+//     res.redirect("/profile");
+//     return;
+//   }
+
+//   res.render("login");
+// });
 
 module.exports = router;
