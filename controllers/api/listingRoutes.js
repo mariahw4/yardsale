856b6a392b82ceee5c287dec77dd5974b365cc51
rhubarb414 <ctophr.dubois@gmail.com>
2023-03-29 08:32:01 -0700

    initial offering: /listings/:id renders a single listing. Still missing some data, and the purchase button

--- controllers/api/listingRoutes.js
+++ controllers/api/listingRoutes.js
@@ -43,6 +43,8 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Draft 2 of listing detail route
+
 router.get("/:id", async (req, res) => {
   try {
     const listingData = await Listing.findOne({
@@ -63,28 +65,72 @@ router.get("/:id", async (req, res) => {
           model: User,
           attributes: ["name"],
         },
-
-        // ~~~ For comments feature. Turned off for now ~~~
-        // {
-        //   model: Comment,
-        //   attributes: ["id", "comment_content", "blog_id", "user_id"],
-        //   include: {
-        //     model: User,
-        //     attributes: ["name"],
-        //   },
-        // },
       ],
     });
+
     if (!listingData) {
       res.status(404).json({ message: "No listing found with this id!" });
       return;
     }
+    // Serialize data so the handlebars template can read it
+    const listing = listingData.get({ plain: true });
+    console.log("Listing:", listing); // delete for deploy
 
-    res.status(200).json(listingData);
+    // Pass serialized data and session flag into template
+    res.status(200).render("listing", {
+      listing,
+      // loggedIn: req.session.loggedIn,
+    });
   } catch (err) {
+    console.log("catch called");
     res.status(500).json(err);
   }
 });
+
+// ~~~ original get route. returns the object~~~
+
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const listingData = await Listing.findOne({
+//       where: {
+//         id: req.params.id,
+//       },
+//       attributes: [
+//         "id",
+//         "title",
+//         "description",
+//         "price",
+//         "image",
+//         "date_created",
+//       ],
+
+//       include: [
+//         {
+//           model: User,
+//           attributes: ["name"],
+//         },
+
+//         // ~~~ For comments feature. Turned off for now ~~~
+//         // {
+//         //   model: Comment,
+//         //   attributes: ["id", "comment_content", "blog_id", "user_id"],
+//         //   include: {
+//         //     model: User,
+//         //     attributes: ["name"],
+//         //   },
+//         // },
+//       ],
+//     });
+//     if (!listingData) {
+//       res.status(404).json({ message: "No listing found with this id!" });
+//       return;
+//     }
+
+//     res.status(200).json(listingData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.post("/", withAuth, async (req, res) => {
   try {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,14 +8,14 @@ const withAuth = require("../utils/auth");
 //   res.json("Home route");
 // });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all listings and JOIN with user data
     const listingData = await Listing.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
@@ -25,9 +25,9 @@ router.get('/', async (req, res) => {
     console.log("Listings:", listings);
 
     // Pass serialized data and session flag into template
-    res.render('homepage', {
+    res.render("homepage", {
       listings,
-      loggedIn: req.session.loggedIn
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -43,6 +43,5 @@ router.get("/login", (req, res) => {
 
   res.render("login");
 });
-
 
 module.exports = router;
